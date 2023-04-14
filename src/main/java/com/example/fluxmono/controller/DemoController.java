95be97eaf46ff6c43d58502233ce2e9076ffd46b
aungmyothan5715 <aungmyothan571@gmail.com>
2023-04-14 10:13:16 +0630

    Publisher with Mono

--- src/main/java/com/example/fluxmono/controller/DemoController.java
+++ src/main/java/com/example/fluxmono/controller/DemoController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.util.stream.Stream;
@@ -15,8 +16,10 @@ public class DemoController {
     public Publisher<Integer> demo(){
         //Publisher is as the same Mono and Flux.
         //Publisher is called Mono and Flux.
-        return Flux.fromStream(
-                Stream.of(1, 2, 3, 4, 5, 6 ,7, 8, 9)
-        ).delayElements(Duration.ofSeconds(4));
+//        return Flux.fromStream(
+//                Stream.of(1, 2, 3, 4, 5, 6 ,7, 8, 9)
+//        ).delayElements(Duration.ofSeconds(4));
+
+        return Mono.just(9).delayElement(Duration.ofSeconds(3)); // Mono phyit lo Mono.just(8) data one lone pae ya tal.
     }
 }
